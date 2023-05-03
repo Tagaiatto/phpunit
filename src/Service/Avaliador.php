d6d2df7f9b1967b22617e76070cebbb7e0352e0f
Tagaiatto <edilsonmneto@gmail.com>
2023-05-03 20:50:24 -0300

    add Avaliador service

---
+++ src/Service/Avaliador.php
@@ -0,0 +1,16 @@
+<?php
+namespace Alura\Leilao\Service;
+
+use Alura\Leilao\Model\Leilao;
+
+class Avaliador
+{
+    private $maiorValor;
+
+    public function avalia(Leilao $leilao): void
+    {
+        
+    }
+
+    
+}
